f8b1a09503063f4b0b86f58c0f078c232a0fcaaf
Oleg Shnitko <olegshnitko@gmail.com>
2010-05-20 13:02:54 +0300

    Added global kit header

---
+++ Classes/MaptimizeKit.h
@@ -0,0 +1,34 @@
+//
+//  MaptimizeKit.h
+//  MaptimizeKit
+//
+//  Created by Oleg Shnitko on 5/20/10.
+//  olegshnitko@gmail.com
+//  
+//  Copyright © 2010 Screen Customs s.r.o. All rights reserved.
+//
+
+// model
+#import "XMPlacemark.h"
+#import "XMCluster.h"
+#import "XMMarker.h"
+#import "XMGraph.h"
+
+// network
+#import "XMNetworkErrors.h"
+#import "XMBounds.h"
+#import "XMCondition.h"
+#import "XMRequest.h"
+#import "XMClusterizeRequest.h"
+#import "XMSelectRequest.h"
+#import "XMOptimizeService.h"
+
+// tiling
+#import "XMTile.h"
+#import "XMTileCache.h"
+#import "XMTileService.h"
+
+// map
+#import "XMMapController.h"
+#import "XMMercatorProjection.h"
+#import "XMClusterView.h"
